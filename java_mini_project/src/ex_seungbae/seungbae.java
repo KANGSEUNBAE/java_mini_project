23f4ba2fcae097123f646e656991054db727fbb8
KANGSEUNBAE <KANGSEUNGBAE00@gmail.com>
2024-04-24 16:40:56 +0900

    kangseungbae

--- java_mini_project/src/ex_seungbae/seungbae.java
+++ java_mini_project/src/ex_seungbae/seungbae.java
@@ -1,12 +1,23 @@
 package ex_seungbae;
 
+import java.util.Scanner;
+
 public class seungbae {
 
-	public static void main(String[] args) {
-	
-		System.out.println("강승배");
-		
-		
+	public static void main(String[] args)
+	{
+		Scanner scanner = new Scanner(System.in);
+		String lunch[] = new String[4];
+		System.out.println("메뉴입력하세요");
+		for (int i = 0; i < lunch.length; i++) {
+			lunch[i] = scanner.next();
+		}
+		System.out.println("메뉴리스트");
+		for (int i = 0; i < lunch.length; i++) {
+			
+			System.out.println(" " + lunch[i]);
+			scanner.close();
+		}
 	}
-	
+
 }
